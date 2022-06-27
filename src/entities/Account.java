1ff990f49daa74523769e25cef775f9ef07f6067
Felipe Vieira <felipefernandes.contato09@gmail.com>
2022-06-27 12:20:59 -0300

    Adicionado todas as funções

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,10 +1,42 @@
 package entities;
 
 public class Account {
-    private int actNumber;
-    private String actName;
+    private int number;
+    private String holder;
     private double balance;
 
+    public Account(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
 
+    public int getNumber() {
+        return number;
+    }
 
+    public String getHolder() {
+        return holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance += - amount - 5;
+    }
+
+    public String toString() {
+        return "Account " + number +
+                ", Holder: " + holder +
+                ", Balance: $ " + balance;
+    }
 }
